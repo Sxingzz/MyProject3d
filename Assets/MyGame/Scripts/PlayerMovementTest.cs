07605a7a30e0f291b7fc39c138957881614c9c76
MAY8\MAY08 <saohuynh223@gmail.com>
2023-11-08 21:34:45 +0700

    [add] simple player movement

---
+++ Assets/MyGame/Scripts/PlayerMovementTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementTest : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private float turnSpeed; // tốc độ xoay
+
+    private float horizontalInput;
+    private float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+
+        Vector3 movementDirection = new Vector3 (horizontalInput, 0, verticalInput);
+        
+        print($"Vector Magnitude Before normalize: {movementDirection.magnitude}");
+        movementDirection.Normalize(); // chuẩn hóa vector để khi đi xéo vẫn giữ tốc độ là 1
+        print($"Vector Magnitude After normalize: {movementDirection.magnitude}");
+
+        transform.Translate(movementDirection * Time.deltaTime, Space.World);
+
+        if(movementDirection != Vector3.zero ) // hướng của charactor
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, turnSpeed * Time.deltaTime);
+        }
+    }
+}
