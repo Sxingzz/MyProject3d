aadb9b3b060a7397acb2e6fae4936dad08bd3d48
MAY8\MAY08 <saohuynh223@gmail.com>
2023-11-20 21:26:38 +0700

    [add] Ragdoll

---
+++ Assets/MyGame/Scripts/ShootTest.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootTest : MonoBehaviour
+{
+    [SerializeField]
+    private float maximumForce;
+
+    [SerializeField]
+    private float maximumForceTime;
+
+    private float timeMouseButtonDown; // thời gian giữ chuột trái
+
+    private Camera mainCamera;
+
+
+  
+    void Awake()
+    {
+        mainCamera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            timeMouseButtonDown = Time.time;
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition); // tạo 1 cái tia từ input.mouseposition
+
+            if(Physics.Raycast(ray, out RaycastHit hitInfo))
+            {
+                ZombieTest  zombie = hitInfo.collider.GetComponentInParent<ZombieTest>();
+
+                if(zombie != null)
+                {
+                    float mouseButtonDownDuration = Time.time - timeMouseButtonDown; // thời gian giữ chuột
+                    float forcePercentage = mouseButtonDownDuration / maximumForceTime;
+                    float forceMagnitude = Mathf.Lerp(1, maximumForce, forcePercentage); // Lerp: nội suy tuyến tính
+
+                    Vector3 forceDirection = zombie.transform.position - mainCamera.transform.position;
+                    forceDirection.y = 1; // Khi bắn vào con zombie lên cao 1 xíu
+                    forceDirection.Normalize();
+
+                    Vector3 force = forceMagnitude * forceDirection;
+
+                    zombie.TriggerRagdoll(force, hitInfo.point);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/MyGame/Scripts/ZombieTest.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ZombieTest : MonoBehaviour
+{
+    private enum ZombieState
+    {
+        Walking,
+        Ragdoll
+    }
+    [SerializeField]
+    private Camera mainCamera;
+
+    private Rigidbody[] zombieRBs;
+    private CharacterJoint[] zombieJoint;
+    private ZombieState currentState = ZombieState.Walking;
+    private Animator zombieAnimator;
+    private CharacterController zombieCC;
+
+
+
+    private void Awake()
+    {
+        zombieRBs = GetComponentsInChildren<Rigidbody>();
+        zombieJoint = GetComponentsInChildren<CharacterJoint>();
+        zombieAnimator = GetComponent<Animator>();
+        zombieCC = GetComponent<CharacterController>();
+        DisableRagdoll();
+        SetUpCHaracterJoint();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       switch(currentState)
+        {
+            case ZombieState.Walking:
+                WalkingBehavior();
+                    break;
+            case ZombieState.Ragdoll:
+                RagdollBehavior(); 
+                    break;
+        }
+    }
+    private void SetUpCHaracterJoint()
+    {
+        foreach (var joint in zombieJoint)
+        {
+            joint.enableProjection = true;
+        }
+    }
+
+    private void DisableRagdoll()
+    {
+        foreach (var rigidbody in zombieRBs)
+        {
+            rigidbody.isKinematic = true;
+        }
+
+        zombieAnimator.enabled = true;
+        zombieCC.enabled = true;
+    }
+
+    private void EnableRagdoll()
+    {
+        foreach (var rigidbody in zombieRBs)
+        {
+            rigidbody.isKinematic = false;
+        }
+
+        zombieAnimator.enabled = false;
+        zombieCC.enabled = false;
+    }
+
+    private void WalkingBehavior()
+    {
+        Vector3 direction = mainCamera.transform.position - transform.position;
+        direction.y = 0;
+        direction.Normalize();
+
+        Quaternion toRotation = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, 20 * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            EnableRagdoll();
+            currentState = ZombieState.Ragdoll;
+        }
+    }
+
+    private void RagdollBehavior()
+    {
+        
+    }
+
+    public void TriggerRagdoll(Vector3 force, Vector3 hitPoint)
+    {
+        EnableRagdoll();
+
+        Rigidbody hitRB = zombieRBs.OrderBy(Rigidbody => Vector3.Distance(Rigidbody.position, hitPoint)).First();
+
+        hitRB.AddForceAtPosition(force, hitPoint, ForceMode.Impulse); // ForceMode.Impulse: tác động liền lập tức
+
+    }
+   
+}
