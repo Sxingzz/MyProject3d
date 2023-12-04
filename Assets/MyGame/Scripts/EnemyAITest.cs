4f2b6263802a6151a74591becc45020161d43b68
MAY8\MAY08 <saohuynh223@gmail.com>
2023-12-04 21:29:16 +0700

    [add] AI

---
+++ Assets/MyGame/Scripts/EnemyAITest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAITest : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform playerTF;
+
+    private NavMeshAgent navMeshAgent;
+
+    private void Awake()
+    {
+        playerTF = GameObject.FindGameObjectWithTag("Player").transform;
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.SetDestination(playerTF.position); // di chuyển tự động tránh các vật thể
+    }
+}
