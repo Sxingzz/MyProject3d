d0a37b3a69d265ed27579b59337e3375f488614c
MAY8\MAY08 <saohuynh223@gmail.com>
2023-12-15 21:33:05 +0700

    [add] package to make loading UI

---
+++ Assets/MyGame/Scripts/UI/PopupSetting.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupSetting : BasePopup
+{
+    public override void Init()
+    {
+        base.Init();
+    }
+    public override void Show(object data)
+    {
+        base.Show(data);
+    }
+
+    public override void Hide()
+    {
+        base.Hide(); 
+    }
+
+    public void OnClickApplyButton()
+    {
+        Hide();
+    }
+
+    public void OnCanvasGroupChanged()
+    {
+        Hide();
+    }
+
+
+}
